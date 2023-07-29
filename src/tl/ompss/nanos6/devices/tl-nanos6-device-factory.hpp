aec3bcc5036e50e5a58b4d40a88f89786897161f
iBug <git@ibugone.com>
2023-07-29 16:21:52 +0800

    misc: Change shared_ptr(new) -> make_shared()

--- src/tl/ompss/nanos6/devices/tl-nanos6-device-factory.hpp
+++ src/tl/ompss/nanos6/devices/tl-nanos6-device-factory.hpp
@@ -1,23 +1,23 @@
 /*--------------------------------------------------------------------
   (C) Copyright 2015-2015 Barcelona Supercomputing Center
                           Centro Nacional de Supercomputacion
-  
+
   This file is part of Mercurium C/C++ source-to-source compiler.
-  
+
   See AUTHORS file in the top level directory for information
   regarding developers and contributors.
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 3 of the License, or (at your option) any later version.
-  
+
   Mercurium C/C++ source-to-source compiler is distributed in the hope
   that it will be useful, but WITHOUT ANY WARRANTY; without even the
   implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
   PURPOSE.  See the GNU Lesser General Public License for more
   details.
-  
+
   You should have received a copy of the GNU Lesser General Public
   License along with Mercurium C/C++ source-to-source compiler; if
   not, write to the Free Software Foundation, Inc., 675 Mass Ave,
@@ -45,15 +45,15 @@ namespace TL { namespace Nanos6 {
             {
                 if (device_name == "smp")
                 {
-                    return std::shared_ptr<SMPDevice>(new SMPDevice());
+                    return std::make_shared<SMPDevice>();
                 }
                 else if (device_name == "cuda")
                 {
-                    return std::shared_ptr<CUDADevice>(new CUDADevice());
+                    return std::make_shared<CUDADevice>();
                 }
                 else if (device_name == "openacc")
                 {
-                    return std::shared_ptr<OpenACCDevice>(new OpenACCDevice());
+                    return std::make_shared<OpenACCDevice>();
                 }
                 else
                 {
